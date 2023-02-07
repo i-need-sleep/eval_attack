657554beea345eb6d867497be6b3e714cf1e5b0d
yichen huang <yh2689@nyu.edu>
2023-02-07 21:32:10 +0400

    sh

--- sh/poc_bleurt_down.sh
+++ sh/poc_bleurt_down.sh
@@ -3,15 +3,13 @@
 #SBATCH --nodes=1                   # 这里不用动 多节点脚本请查官方文档
 #SBATCH --ntasks=1                  # 这里不用动 多任务脚本请查官方文档
 #SBATCH --cpus-per-task=4           # 要几块CPU (一般4块就够用了)
-#SBATCH --mem=256GB                 # 最大内存
+#SBATCH --mem=64GB                 # 最大内存
 #SBATCH --time=12:00:00           # 运行时间上限
 #SBATCH --mail-type=END             # ALL / END
 #SBATCH --mail-user=yh2689@nyu.edu  # 结束之后给哪里发邮件
 #SBATCH --output=./logs/poc/%x%A.out           # 正常输出写入的文件
 #SBATCH --error=./logs/poc/%x%A.err            # 报错信息写入的文件
-#SBATCH --gres=gpu:1                # 需要几块GPU (同时最多8块)
-#SBATCH -p gpu                   # 有GPU的partition
-#SBATCH --qos=gpu-8                 # To enable the use of up to 8 GPUs
+#SBATCH -q cpu-512                  # 有GPU的partition
 
 nvidia-smi
 nvcc --version
